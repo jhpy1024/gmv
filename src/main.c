69bc5823c4bdcf662c836cd89e923a9144e506b6
jhpy1024 <jhpy1024@gmail.com>
2015-03-06 19:35:34 +0000

    Created function for getting the number of files in a directory

--- src/main.c
+++ src/main.c
@@ -15,6 +15,20 @@ DIR* open_directory(const char* dir_name)
     return dir;
 }
 
+unsigned num_files_in_directory(DIR* dir)
+{
+    unsigned num_files = 0;
+
+    struct dirent* file;
+    while ((file = readdir(dir)) != NULL)
+    {
+        ++num_files;
+    }
+
+    rewinddir(dir);
+    return num_files;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 4)
