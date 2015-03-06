8bef357839a7106abe9df3081892ae45a7293fd1
jhpy1024 <jhpy1024@gmail.com>
2015-03-06 19:22:19 +0000

    Created function for opening directories and handling any possible errors

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
+#include <error.h>
+#include <errno.h>
 #include <stdio.h>
+#include <dirent.h>
 
-int main()
+DIR* open_directory(const char* dir_name)
 {
-    puts("Hello World!");
+    DIR* dir = opendir(dir_name);
+
+    if (dir == NULL)
+    {
+        error(1, errno, "An error occured when trying to open the directory \"%s\"", dir_name);
+    }
+
+    return dir;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s <extension> <from> <to>\n", argv[0]);
+        return 1;
+    }
+
+    DIR* from_dir = open_directory(argv[2]);
+    
     return 0;
 }
